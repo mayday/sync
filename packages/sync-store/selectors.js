cf6dfec85c5011e56cf44a586e3733bfb444df33
case <case@casesandberg.com>
2016-12-12 18:40:19 -0500

    Update `redux-selector` to 0.2.0

--- packages/sync-store/selectors.js
+++ packages/sync-store/selectors.js
@@ -4,13 +4,12 @@ import { selectors as git } from 'sync-store-git'
 import { selectors as settings } from 'sync-store-settings'
 import { selectors as ui } from 'sync-store-ui'
 
-export default {
-  ...scopeStateToSelectors({
-    git,
-    settings,
-    ui,
-  }),
+export default scopeStateToSelectors({
+  git,
+  settings,
+  ui,
+}, {
   getCurrentRepo: state => git.getRepoByPath(state.git, ui.getActiveRepo(state.ui)),
   getCurrentCommits: state => git.getCommitsByRepo(state.git, ui.getActiveRepo(state.ui)),
   getReposStatuses: state => git.getReposByPaths(state.git, settings.getReposPaths(state.settings)),
-}
+})
