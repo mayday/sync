3b816e652c4832987b2cbc83dd5224312547e71b
case <case@casesandberg.com>
2016-12-15 14:45:38 -0500

    Remove Container File

--- packages/sync-pane-branches/index.js
+++ packages/sync-pane-branches/index.js
@@ -1,3 +1,25 @@
-export { default } from './container'
+import { connect } from 'react-redux'
+import { store } from 'sync-store'
+import { actions } from './reducer'
+
+import { Bar } from './bar'
+
+export default connect(
+  state => ({
+    name: 'sync',
+    current: store.getCurrentBranch(state),
+    branches: store.getFilteredBranches(state),
+    listVisible: store.getListVisibility(state),
+    search: store.getSearch(state),
+    active: store.getActive(state),
+  }),
+  {
+    onToggleList: actions.toggleListVisibility,
+    onSearch: actions.setSearch,
+    onClear: actions.clear,
+    onSelect: actions.select,
+    onSetActive: actions.setActive,
+  },
+)(Bar)
 
 export { reducer, actions, selectors } from './reducer'
