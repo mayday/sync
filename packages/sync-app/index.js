1beebbaa043fe8c3ab390a22f2a5e4b0f0603681
case <case@casesandberg.com>
2016-12-17 01:04:40 -0500

    Don't Import Electron into Webpack

--- packages/sync-app/index.js
+++ packages/sync-app/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { configureStore } from 'sync-store'
-import { getSettings } from 'sync-electron'
+// import { getSettings } from 'sync-electron'
 import App from './components/App'
 
-const store = configureStore({ settings: getSettings() })
+const store = configureStore({ settings: {} })
 
 render(
   <Provider store={ store }>
