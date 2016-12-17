afc1257879cf7cb2ea4455a2c8c2372c5047813e
case <case@casesandberg.com>
2016-12-17 16:00:00 -0500

    Init Local Changes and Commits Plugins

---
+++ packages/sync-plugin-commits/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Icon } from 'sync-components'
+
+export const name = 'Commits'
+export const icon = <Icon name="history" />
+
+export default <div>Commits</div>
---
+++ packages/sync-plugin-local-changes/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Icon } from 'sync-components'
+
+export const name = 'Local Changes'
+export const icon = <Icon name="checkbox-multiple-marked" />
+
+export default <div>Local Changes</div>
