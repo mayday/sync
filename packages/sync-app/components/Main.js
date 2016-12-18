38710fb85a06d5f0b94ea8c063521a4e8aeb5aa6
case <case@casesandberg.com>
2016-12-18 15:20:00 -0500

    Remove Old CommitList Code

--- packages/sync-app/components/Main.js
+++ packages/sync-app/components/Main.js
@@ -1,57 +1,33 @@
 import React from 'react'
-import _ from 'lodash'
 import reactCSS from 'reactcss'
-
-import { connect } from 'react-redux'
-import { store } from 'sync-store-selectors'
-import { actions } from 'sync-store-git'
 
 import CommitsPlugin from 'sync-plugin-commits'
 
 import { Scroll } from 'sync-components'
 
-export class Main extends React.Component {
-  componentDidUpdate(lastProps) {
-    if (lastProps.path !== this.props.path) {
-      this.props.gitAdd().then(() => {
-        this.props.gitDiff()
-        this.props.gitDiffSummary()
-      })
-      this.props.gitCommits()
-      this.props.gitStatus()
-    }
-  }
-
-  render() {
-    const styles = reactCSS({
-      'default': {
-        main: {
-          marginTop: 15,
-          marginRight: 15,
-          marginLeft: 5,
-        },
-        changes: {
-          marginBottom: 15,
-          maxHeight: '78vh',
-          display: 'flex',
-        },
+export const Main = () => {
+  const styles = reactCSS({
+    'default': {
+      main: {
+        marginTop: 15,
+        marginRight: 15,
+        marginLeft: 5,
       },
-    })
+      changes: {
+        marginBottom: 15,
+        maxHeight: '78vh',
+        display: 'flex',
+      },
+    },
+  })
 
-    return (
-      <Scroll scrollToBottom>
-        <div style={ styles.main }>
-          <CommitsPlugin />
-        </div>
-      </Scroll>
-    )
-  }
+  return (
+    <Scroll scrollToBottom>
+      <div style={ styles.main }>
+        <CommitsPlugin />
+      </div>
+    </Scroll>
+  )
 }
 
-export default connect(
-  state => ({
-    ...store.getCurrentRepo(state),
-    commits: _.reverse(store.getCurrentCommits(state) || []),
-  }),
-  actions,
-)(Main)
+export default Main
--- packages/sync-components/commit-list/helper.js
+++
@@ -1,8 +0,0 @@
-export const parseMessageText = text =>
-  text.replace(/ \(.+?(->|\/).+?\)/, '')
-
-export const loop = (i, length) => {
-  const first = i === 0
-  const last = i === length - 1
-  return { position: (first && 'first') || (last && 'last') || 'middle' }
-}
